dff63a82e23c35b39199985e4984b0ab040374ee
ruhyat <marullahruhyat@gmail.com>
2025-11-27 16:35:10 +0700

    27 nov 2025

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "api-game.ruhyat.com", "127.0.0.1"],
+    domains: [
+      "localhost",
+      "api-game.ruhyat.com",
+      "127.0.0.1",
+      "file-game.ruhyat.com",
+    ],
   },
 };
 
